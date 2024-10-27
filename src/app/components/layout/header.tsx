d98d0228a86b917e5b41016335cdbada1cdda007
Aliza Faizan <alizafaizan86@gmail.com>
2024-10-28 02:24:10 +0500

    add

--- src/app/components/layout/header.tsx
+++ src/app/components/layout/header.tsx
@@ -1,17 +1,13 @@
 import { MenuIcon } from "lucide-react";
 import { FaCartArrowDown } from "react-icons/fa";
-import {
-    Sheet,
-    SheetContent,
-    SheetTrigger,
-  } from "@/components/ui/sheet";
-  
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 
 export default function Header() {
   return (
-    <header className="bg-slate-800 shadow-md mt-2 py-5">
+    <header className="bg-slate-800 shadow-md py-5">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-2xl font-bold text-white">ZFClothes</h1>
+      <img src="/logo2.jpg" alt="logo" className="w-10 h-10 rounded-full "/>
+       <h1 className="text-2xl font-bold text-white ml-2 ">ZFClothes </h1>
         <div className="hidden md:block">
           <nav className="flex items-center space-x-6">
             <a
@@ -34,29 +30,32 @@ export default function Header() {
         </div>
         <Sheet>
           <SheetTrigger className="md:hidden">
-            <MenuIcon  className="text-white"/>
+            <MenuIcon className="text-white" />
           </SheetTrigger>
           <SheetContent>
-          <div className="hidden md:block">
-          <nav className="flex items-center space-x-6">
-            <a
-              href="#"
-              className="text-lg font-medium text-gray-700 hover:text-blue-600"
-            >
-              New Arrivals
-            </a>
-            <a
-              href="#"
-              className="ml-6 text-lg font-medium text-gray-700 hover:text-blue-600"
-            >
-              Cart
-            </a>
-            {/* cart icon */}
-            <a href="#" className="text-lg text-gray-700 hover:text-blue-600">
-              <FaCartArrowDown className="h-6 w-6" />
-            </a>
-          </nav>
-        </div>
+            <div className="hidden md:block">
+              <nav className="flex items-center space-x-6">
+                <a
+                  href="#"
+                  className="text-lg font-medium text-gray-700 hover:text-blue-600"
+                >
+                  New Arrivals
+                </a>
+                <a
+                  href="#"
+                  className="ml-6 text-lg font-medium text-gray-700 hover:text-blue-600"
+                >
+                  Cart
+                </a>
+                {/* cart icon */}
+                <a
+                  href="#"
+                  className="text-lg text-gray-700 hover:text-blue-600"
+                >
+                  <FaCartArrowDown className="h-6 w-6" />
+                </a>
+              </nav>
+            </div>
           </SheetContent>
         </Sheet>
       </div>
